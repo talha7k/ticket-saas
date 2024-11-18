5d345b85183de9cf8c855943d64ac99cecfe2a8f
Talha Khan <talha7k@gmail.com>
2024-11-18 16:07:38 +0400

    Simplified Routing & Header Buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,20 @@ function Header() {
 
       <div>
         <SignedIn>
-          <UserButton />
+          <div className="flex items-center">
+            <Link href="/seller">
+              <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition mr-4">
+                Sell Tickets
+              </button>
+            </Link>
+
+            <Link href="/my-tickets">
+              <button className="bg-gray-100 text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition mr-4 border border-gray-300">
+                My Tickets
+              </button>
+            </Link>
+            <UserButton />
+          </div>
         </SignedIn>
 
         <SignedOut>
