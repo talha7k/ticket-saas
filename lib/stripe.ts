a5ad5c468db9f8071280cadab6c3123d66994c13
Talha Khan <talha7k@gmail.com>
2024-11-21 00:15:08 +0400

    updated stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,5 +5,5 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2023-08-16", // Use the latest API version
+  apiVersion: "2024-10-28.acacia",
 });
